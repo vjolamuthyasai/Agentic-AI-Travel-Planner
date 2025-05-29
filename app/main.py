0ee67f0fb60df1a22bcb5ba972051a2e6114cbf9
vjolamuthyasai <99132520+vjolamuthyasai@users.noreply.github.com>
2025-05-29 22:13:22 +0530

    feat: Added The initial setup of project created folder structure (#2)

---
+++ app/main.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+from app.api.routes import router
+
+def create_app():
+    app = FastAPI(title="Agentic AI Travel Planner")
+    app.include_router(router)
+    return app
+
+
+
