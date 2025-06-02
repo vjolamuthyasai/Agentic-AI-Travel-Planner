4943a292d8b7ec06bd4e2c4a6864cf775de8365a
vjolamuthyasai <99132520+vjolamuthyasai@users.noreply.github.com>
2025-06-02 23:09:57 +0530

    Agentic AI Integration For Hotel Search (#7)
    * feat: Searches for ai recommendations
    
    * feat: Searches for ai recommendations

--- app/models/hotel/hotelInfo.py
+++ app/models/hotel/hotelInfo.py
@@ -1,8 +1,60 @@
+from typing import Optional, List, Dict
 from pydantic import BaseModel
 
+
+class GPSCoordinates(BaseModel):
+    latitude: Optional[float] = None
+    longitude: Optional[float] = None
+
+class Rate(BaseModel):
+    lowest: Optional[str] = None
+    extracted_lowest: Optional[int] = None
+    before_taxes_fees: Optional[str] = None
+    extracted_before_taxes_fees: Optional[int] = None
+
+class Transportation(BaseModel):
+    type: Optional[str] = None
+    duration: Optional[str] = None
+
+class NearbyPlace(BaseModel):
+    name: Optional[str] = None
+    transportations: Optional[List[Transportation]] = []
+
+class Image(BaseModel):
+    thumbnail: Optional[str] = None
+    original_image: Optional[str] = None
+
+class Rating(BaseModel):
+    stars: Optional[int] = None
+    count: Optional[int] = None
+
+class ReviewBreakdown(BaseModel):
+    name: Optional[str] = None
+    description: Optional[str] = None
+    total_mentioned: Optional[int] = None
+    positive: Optional[int] = None
+    negative: Optional[int] = None
+    neutral: Optional[int] = None
+
 class HotelInfo(BaseModel):
-    name: str
-    price: str
-    rating: float
-    location: str
-    link: str
+    type: Optional[str] = None
+    name: Optional[str] = None
+    description: Optional[str] = None
+    link: Optional[str] = None
+    property_token: Optional[str] = None
+    serpapi_property_details_link: Optional[str] = None
+    gps_coordinates: Optional[GPSCoordinates] = None
+    check_in_time: Optional[str] = None
+    check_out_time: Optional[str] = None
+    rate_per_night: Optional[Rate] = None
+    total_rate: Optional[Rate] = None
+    nearby_places: Optional[List[NearbyPlace]] = []
+    hotel_class: Optional[str] = None
+    extracted_hotel_class: Optional[int] = None
+    images: Optional[List[Image]] = []
+    overall_rating: Optional[float] = None
+    reviews: Optional[int] = None
+    ratings: Optional[List[Rating]] = []
+    location_rating: Optional[float] = None
+    reviews_breakdown: Optional[List[ReviewBreakdown]] = []
+    amenities: Optional[List[str]] = []
